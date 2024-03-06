fe70ccb07147df94aa570325d380d6bccba03b5f
Minnie860711 <milly86711@gmail.com>
2024-03-06 15:56:29 +0800

    original

---
+++ Minnie_ReadingPost/Minnie_ReadingPost/Models/Class.cs
@@ -0,0 +1,15 @@
+﻿namespace Minnie_ReadingPost.Models
+{
+    public class BaseBE //底層共用class
+    {
+        /// <summary>
+        /// 使用者ID
+        /// </summary>
+        public string UserID { get; set; } = string.Empty;
+        /// <summary>
+        /// 使用者名稱
+        /// </summary>
+        public string UserName { get; set; } = string.Empty;
+
+    }
+}
